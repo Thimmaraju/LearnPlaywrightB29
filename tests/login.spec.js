597032d8ee4d383156f5b60ab54cd1d9f0841d99
G Thimmaraju <thimmaraju418@gmail.com>
2025-06-05 09:56:19 +0530

    framework

---
+++ tests/login.spec.js
@@ -0,0 +1,71 @@
+import { test, expect } from '@playwright/test';
+
+
+test("Verify Login with Valid Credential", async ({page})=>{
+
+    //Actions 
+    await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+
+    await page.locator(`input[name='username']`).fill("Admin")
+
+    await page.locator("input[type='password']").fill("admin123")
+
+    await page.locator("button[type='submit']").click()
+
+    // Assertions
+    await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index')
+  
+    ornum = 2324456
+
+})
+
+
+test("Verify Login with Valid Username and Invalid Password", async ({page})=>{
+
+    //Actions 
+    await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+
+    await page.locator("input[name='username']").fill("Admin")
+
+    await page.locator("input[type='password']").fill("dfjrn")
+
+    await page.locator("button[type='submit']").click()
+
+    // Assertions
+    
+    await expect(page.locator("//p[text()='Invalid credentials']")).toBeVisible()
+})
+
+
+
+test("Verify login with Invalid Username and InValid Password", async ({page})=>{
+
+    //Actions 
+    await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+
+    await page.locator("input[name='username']").fill("bjhbhj")
+
+    await page.locator("input[type='password']").fill("gnjr")
+
+    await page.locator("button[type='submit']").click()
+
+    // Assertions
+    
+    await expect(page.locator("//p[text()='Invalid credentials']")).toBeVisible()
+})
+
+test("Verify login with Invalid Username and Valid Password", async ({page})=>{
+
+    //Actions 
+    await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+
+    await page.locator("input[name='username']").fill("dsfesf")
+
+    await page.locator("input[type='password']").fill("admin123")
+
+    await page.locator("button[type='submit']").click()
+
+    // Assertions
+    
+    await expect(page.locator("//p[text()='Invalid credentials']")).toBeVisible()
+})
