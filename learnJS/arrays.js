69080869788ba7629f7303b346ff06d9d0a59b37
G Thimmaraju <thimmaraju418@gmail.com>
2025-06-19 13:04:26 +0530

    arrays

---
+++ learnJS/arrays.js
@@ -0,0 +1,165 @@
+
+// arr  = []  // empty 
+
+// arr1 =["Raju",4,5,6,7,8,9,4,"shiva",7]
+
+// console.log(arr1.length)
+
+// // index always starts 0 
+// // 1st element = 0 index 
+
+// console.log(arr1[8])
+
+// creds = ["Admin", "admin123"]
+
+// //creds[0]
+
+// console.log(typeof(arr1[0]))
+
+// arr2 = [9,3,5,2,7,4]
+
+// arr3 = arr2.sort()
+
+// console.log(arr3)
+
+
+// arr4 = ['jamuna', "ashwini", "tejaswini", "Anjali", "purushottam", "shiva", "Gagan"]
+
+// // arr5 = arr4.sort()
+
+// // console.log(arr5)
+
+// arr6 = arr4.reverse()
+
+// console.log(arr6)
+
+// str = arr4.join(" ")
+
+// console.log(str)
+
+// str = "this is javascript class"
+
+// arr7 = str.split(" ")
+
+// console.log(arr7)
+
+// // push 
+// // shift
+// // unshift
+// // pop 
+
+// // str2 = "MohammedGouse"
+
+// // revstr = str2.split("").reverse().length
+
+// // console.log(revstr)
+
+// arr = [ "eat", "sleep"]
+
+// arr.push("study") // add the element at the end of the array
+
+// arr.unshift("drink") // add the element at the beginning of the array
+
+// console.log(arr)
+
+
+// arr1 = ['jamuna', "ashwini", "tejaswini", "Anjali", "purushottam", "shiva", "Gagan"]
+
+// arr1.pop() // remove the element at the end of the array
+
+// arr1.pop()
+// // for (let i=0; i<=arr1.length-1; i++){
+
+// //    arr1.pop()
+// // }
+
+
+// arr1.shift()
+// arr1.shift()
+// console.log(arr1)
+
+// arr3 = [ 'drink', 'eat', 'sleep', 'study' ]
+
+// arr3[2] = "play"
+
+// console.log(arr3)
+
+// //includes()  true or false 
+
+
+// arr3.includes("drink")  // true
+
+// if(arr3.includes("drink")){
+
+//     console.log("element is present")
+// }
+
+// arr3.includes("exersize")  // false
+
+// if(arr3.includes("exersize")){
+//      console.log("element is present")
+// }
+// else{
+//       console.log("element is not present")
+// }
+
+// console.log(arr3.indexOf("study"))
+
+// //concat()
+
+// arr4 = [ 'drink', 'eat', 'sleep', 'study' ]
+
+// arr5 = [ "eat", "sleep"]
+
+// arr6 = arr4.concat(arr5)
+
+// console.log(arr6)
+
+// arr6 =[ 'drink', 'eat', 'sleep', 'study', 'eat', 'sleep' ]
+
+// // arr7 = arr6.slice(0,2)
+
+// // console.log(arr7)
+
+// arr6.splice(4, "Raju")
+
+// console.log(arr6)
+
+//filter()
+// map()
+// reduce()
+
+
+arr6 =[ 'drink', 'eat', 'sleep', 'study', 'eat', 'sleep' ]
+
+// arr6.forEach(element => {
+
+//     console.log("loop starts ")
+    
+//     console.log(element)
+    
+//     console.log("loop Ends ")
+// });
+
+
+
+for(let element of arr6){
+
+        console.log("loop starts ")
+    
+    console.log(element)
+    
+    console.log("loop Ends ")
+
+} 
+
+//add 2 numbers 
+
+
+ arr1 =[2,4,5,6,7,8,9,4,7]
+
+ arr2 = arr1.sort().reverse()
+
+ //strings 
+
+ //number 
